ca55fe0317a3cc402e22bf373fb69d9ad09d225c
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-08-12 08:38:21 -0400

    Create Main.java

---
+++ 1/Top-K-Words-From-a-File/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main{
+
+    public static void main(String[] args){
+		String[] str = {"aa", "bb", "aa", "aa", "cc", "dd", "cc", "bb", "bb", "bb"};
+		System.out.println(TopK(str, 2));	
+
+	}
+	
+	private static String TopK(String[] str, int k){
+	
+		HashMap<WordCount, Integer> map = new HashMap<WordCount, Integer>();
+		
+		for(String s : str){
+			if(map.containsKey(new WordCount(s, 0))){
+			   
+			int count = map.get(new WordCount(s, 0)) + 1;
+	
+			map.remove(new WordCount(s, count-1));
+			map.put(new WordCount(s, count), count);
+		
+			}
+			else{
+			    
+				map.put(new WordCount(s, 1), 1);
+	
+			}
+		}
+		List<WordCount> set = new ArrayList<WordCount>(map.keySet());
+		Collections.sort(set);
+
+		String str2 = "";
+		int i = 0;
+		System.out.println("Word :: Count\n"+"---------------");
+		for(WordCount w : set){
+		    i++;
+			str2 += w.word + "\t   "+ w.count + "\n";//map.get(w) + "  ";
+			if(i==k) break;
+		}
+		return str2;
+	}
+}
