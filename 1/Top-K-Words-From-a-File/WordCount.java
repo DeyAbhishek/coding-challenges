9b3eeb5004cfa3023242d4193545ef2ccacb99bd
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-08-12 08:37:48 -0400

    Create WordCount.java

---
+++ 1/Top-K-Words-From-a-File/WordCount.java
@@ -0,0 +1,27 @@
+
+public class WordCount implements Comparable<WordCount>{
+	public String word;
+	public int count;
+	
+	public WordCount(String str, int count){
+		word = str;
+		this.count = count;
+	}
+	
+	public int hashCode(){
+		return word.length();
+	}
+	public boolean equals(Object ob){
+		if(ob instanceof WordCount){
+			return word.equals(((WordCount) ob).word);
+		}
+		return false;
+	}
+	
+	public int compareTo(WordCount ob){
+		
+			return ob.count - this.count;
+		
+	}
+
+}
