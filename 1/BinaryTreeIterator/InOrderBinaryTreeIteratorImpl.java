4f95ef80e2242250419283b758757145f84b2b94
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-08-31 09:23:36 -0400

    Create InOrderBinaryTreeIteratorImpl.java

---
+++ 1/BinaryTreeIterator/InOrderBinaryTreeIteratorImpl.java
@@ -0,0 +1,52 @@
+ public class InOrderBinaryTreeIteratorImpl implements InOrderBinaryTreeIterator {  
+   Stack<TreeNode> stack = new Stack<TreeNode>();  
+   
+   /** Push node cur and all of its left children into stack */  
+   private void pushLeftChildren(TreeNode cur) {  
+     while (cur != null) {  
+       stack.push(cur);  
+       cur = cur.left;  
+     }  
+   }  
+   
+   /** Constructor */  
+   public InOrderBinaryTreeIterator(TreeNode root) {  
+     pushLeftChildren(root);  
+   }  
+   
+   /** {@inheritDoc} */  
+   @Override  
+   public boolean hasNext() {  
+     return !stack.isEmpty();  
+   }  
+   
+   /** {@inheritDoc} */  
+   @Override  
+   public Integer next() {  
+     if (!hasNext()) {  
+       throw new NoSuchElementException("All nodes have been visited!");  
+     }  
+   
+     TreeNode res = stack.pop();  
+     pushLeftChildren(res.right);  
+   
+     return res.val;  
+   }  
+   
+   @Override  
+   public void remove() {  
+     throw new UnsupportedOperationException("remove() is not supported.");  
+   }  
+ } 
+ // This iterator takes extra spaces for the stack, which is O(h) at worst case, where h is the height of the tree.
+
+
+// With this iterator in hand, an in-order traversal of a binary tree can be implemented as follows.
+ public ArrayList<Integer> inorderTraversal(TreeNode root) {  
+   InOrderBinaryTreeIterator iterator = new InOrderBinaryTreeIteratorImpl(root); 
+   ArrayList<Integer> results = new ArrayList<Integer>();
+   while (iterator.hasNext()) {   
+     results.add(iterator.next());  
+   }   
+   return results;    
+ }
