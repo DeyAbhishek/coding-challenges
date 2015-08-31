9ed20fbf52152a270001878c3c9f467b2271af24
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-08-31 09:13:09 -0400

    Create CircularArray.java

---
+++ 1/CircularArray.java
@@ -0,0 +1,66 @@
+ public class CircularArray<T> implements Iterable<T> {
+
+ private T[] items;
+ private int head = 0;
+
+ public CircularArray(int size) {
+ items = (T[]> new Object[size];
+}
+
+ private int convert(int index) {
+ if (index < 0) {
+ index += items.length;
+ }
+ return (head + index) % items.length;
+ }
+
+ public void rotate(int shiftRight) {
+ head = convert(shiftRight);
+
+ }
+
+ public T get(int i) {
+ if (i < 0 || i >= items.length) {
+throw new java.lang.lndexOutOfBoundsException("...");
+}
+ return items[convert(i)];
+ }
+
+public void set(int i, T item) {
+ items[convert(i)] = item;
+ }
+
+
+
+ public Iterator<T> iteratorQ {
+ return new CircularArrayIterator<T>(this);
+ }
+
+ private class CircularArrayIterator<TI> implements lterator<TI>{
+ /* current reflects the offset from the rotated head, not
+ * from the actual start of the raw array. */
+ private int _current = -1;
+ private TI[] _items;
+
+ public CircularArrayIterator(CircularArray<TI> array){
+ _items = array.items;
+ }
+
+ @Override
+ public boolean hasNextQ {
+ return _current < items.length - 1;
+ }
+
+ @0verride
+ public TI next() {
+ _current++;
+ TI item = (TI) _items[convert(_current)];
+ return item;
+ }
+
+  @Override
+ public void removeQ {
+ throw new UnsupportedOperationException("...");
+ }
+ }
+ }
