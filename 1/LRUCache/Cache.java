df4e2dcce6d874d61f9c5daa9982b4c7b5f141ff
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-10-22 11:40:58 -0400

    Create Cache.java

---
+++ 1/LRUCache/Cache.java
@@ -0,0 +1,46 @@
+public class Node{
+    public String pageNumber;
+    public String pageContent;
+    public Node prev;
+    public Node next;
+}
+
+public Class Cache{
+    private int size;
+    private int count;
+    private HashMap<String, Node> map;
+    private Node head, tail;
+
+    public Cache(int size){
+        this.size = size;
+        count = 0;
+        map = new HashMap<String, Node>();
+        head = null;
+        tail = null;
+    }
+    
+    public String getContent(String pageNumber){
+        if(!map.containsKey(pageNumber)){
+            return null;
+        }
+        Node node = map.get(pageNumber);
+        remove(node);
+        attachFirst(node);
+        return node.pageContent;
+    }
+    
+    private void remove(Node node){
+        if(node.pageNumber == tail.pageNumber){
+            node.prev.next = null;
+            return;
+        } 
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    private void attachFirst(Node node){
+        node.next = head;
+        node.prev = null;
+        head = node;
+    }    
+}
