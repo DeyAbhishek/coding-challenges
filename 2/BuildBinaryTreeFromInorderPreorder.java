02e93314d2ac7217eedc684d3222941b95241d22
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-10-26 12:47:05 -0400

    Update BuildBinaryTreeFromInorderPreorder.java

--- 2/BuildBinaryTreeFromInorderPreorder.java
+++ 2/BuildBinaryTreeFromInorderPreorder.java
@@ -1,3 +1,15 @@
 public class BuildBinaryTreeFromInorderPreorder{
 
+  public Node BuildBinaryTreeFromInorderPreorder(Node[] inorderArray, Node[] preorderArray, int inorderStart, 
+                                                                              int inorderEnd, int preorderIndex){
+    if(inorderStart > inorderEnd) return null;
+    Node root = preorderArray[preorderIndex];
+    if (inorderStart == inorderEnd) return root;
+    int rootIndex = search(inorderArray, root);
+    preorderIndex++;
+    root.left = BuildBinaryTreeFromInorderPreorder(inorderArray, preorderArray, inorderStart, rootIndex - 1, int preorderIndex);
+    root.right = BuildBinaryTreeFromInorderPreorder(inorderArray, preorderArray, rootIndex + 1, inorderEnd, preorderIndex);
+    return root;
+  
+  } 
 }
