735ea9cd0541d0f54f89adbdf98974c798a85055
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-11-19 14:41:52 -0500

    Create Flip1Coin.java

---
+++ 2/Flip1Coin.java
@@ -0,0 +1,30 @@
+// Return maximum number of adjacent pair of coins when one coin is flipped
+
+function solution(A) {  //A -> array of coins
+  var reversed = false, adjacency = 0, i = 1;
+
+  for(i = 1; i < A.length-1; i++) {
+    if(A[i] == A[i-1]) {
+      adjacency++;
+    }
+    else if(A[i-1] == A[i+1] && !reversed) {
+      reversed = true;
+      adjacency += 2;
+      i++;
+    }
+  }
+
+  if(reversed) {
+    if(A[A.length-1] == A[A.length-2]) {
+      adjacency++;
+    }
+  } else {
+    if(A[A.length-1] != A[A.length-2]) {
+      adjacency++;
+    } else if(A[0] != A[1]) {
+      adjacency += 2;
+    }
+  }
+
+  return adjacency;
+}
