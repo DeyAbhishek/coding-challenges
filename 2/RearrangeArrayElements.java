fa25b0a9e59a75d653c69fd9e7b9ab97821d654f
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2016-01-05 15:37:47 -0500

    Create RearrangeArrayElements.java

---
+++ 2/RearrangeArrayElements.java
@@ -0,0 +1,41 @@
+// If [a1,a2,a3...,an,b1,b2...bn] is given input 
+// Change this to [a1,b1,a2,b2.....an,bn] 
+// Solution should be in-place.
+
+
+import java.util.*;
+public class RearrangeArrayElements{
+
+    public static void main(String[] args){
+
+    String[] arr = {"a1", "a2", "a3", "a4", "b1", "b2", "b3", "b4"};
+    	 for(String str : arr){
+    		 System.out.print(str + "   ");
+    	 }
+    	 System.out.println();
+    	 Arrays.sort(arr, new Comparator<String>(){
+    		 @Override
+    		 public int compare(String a, String b){
+    			 int index1 = (int)a.charAt(1);
+    			 int index2 = (int)b.charAt(1);
+    			 if(index1 != index2){
+    				 return index1 - index2;
+    			 }
+    			 else{
+    				 return (int)a.charAt(0) - (int)b.charAt(0);
+    			 }
+    		 }
+    	 });
+    	 for(String str : arr){
+    		 System.out.print(str + "   ");
+    	 }
+    	 System.out.println();
+	}
+    
+    
+    /*
+    OUTPUT:
+    a1   a2   a3   a4   b1   b2   b3   b4   
+    a1   b1   a2   b2   a3   b3   a4   b4   
+
+    */
