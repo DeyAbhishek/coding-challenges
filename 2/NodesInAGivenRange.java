a1ada7c4b4d325c13dd0581fb59fb500e3837404
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2016-01-06 11:01:34 -0500

    Create NodesInAGivenRange.java

---
+++ 2/NodesInAGivenRange.java
@@ -0,0 +1,8 @@
+// Write a function that counts the nodes in BS-tree whose values fit in the given range.
+
+int CountNodesForRange<typename T>(const bsnode<T> *root, T min_range, T max_range) {
+    if (root == NULL) return 0;
+    return CountNodesForRange(root.left, min_range, max_range) +
+           CountNodesForRange(root.right, min_range, max_range) +
+           (root.value >= min_range && root.value <= max_range ? 1 : 0);
+}
