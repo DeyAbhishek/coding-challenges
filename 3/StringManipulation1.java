d4027f937c2f510fe87d92b833e9b1e4eb76b53d
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2016-03-30 18:24:11 -0500

    Update StringManipulation1.java

--- 3/StringManipulation1.java
+++ 3/StringManipulation1.java
@@ -47,7 +47,9 @@ public class StringManipulation1 {
 }
 
 /*
-Julius Caesar protected his confidential information by encrypting it in a cipher. Caesar's cipher rotated every letter in a string by a fixed number, KK, making it unreadable by his enemies. Given a string, SS, and a number, KK, encrypt SS and print the resulting string.
+Julius Caesar protected his confidential information by encrypting it in a cipher. Caesar's cipher rotated every 
+letter in a string by a fixed number, KK, making it unreadable by his enemies. Given a string, SS, and a number, KK, encrypt SS 
+and print the resulting string.
 
 Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
 
@@ -76,13 +78,16 @@ Sample Output
 okffng-Qwvb
 Explanation
 
-Each unencrypted letter is replaced with the letter occurring KK spaces after it when listed alphabetically. Think of the alphabet as being both case-sensitive and circular; if KK rotates past the end of the alphabet, it loops back to the beginning (i.e.: the letter after zz is aa, and the letter after ZZ is AA).
+Each unencrypted letter is replaced with the letter occurring KK spaces after it when listed alphabetically. Think of the alphabet 
+as being both case-sensitive and circular; if KK rotates past the end of the alphabet, 
+it loops back to the beginning (i.e.: the letter after zz is aa, and the letter after ZZ is AA).
 
 Selected Examples: 
 mm (ASCII 109) becomes oo (ASCII 111). 
 ii (ASCII 105) becomes kk (ASCII 107). 
 −− remains the same, as symbols are not encoded. 
 OO (ASCII 79) becomes QQ (ASCII 81). 
-zz (ASCII 122) becomes bb (ASCII 98); because zz is the last letter of the alphabet, aa (ASCII 97) is the next letter after it in lower-case rotation.
+zz (ASCII 122) becomes bb (ASCII 98); because zz is the last letter of the alphabet, aa (ASCII 97) is the next letter after
+it in lower-case rotation.
 
 */
