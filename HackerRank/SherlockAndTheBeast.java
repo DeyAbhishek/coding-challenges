c2e3c882fe62ec7c7fb077da26485d72fb303fa2
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2016-04-05 17:14:34 -0500

    Update SherlockAndTheBeast.java

--- HackerRank/SherlockAndTheBeast.java
+++ HackerRank/SherlockAndTheBeast.java
@@ -38,9 +38,12 @@ public class SherlockAndTheBeast {
     }
     
     /*
-    Sherlock Holmes suspects his archenemy, Professor Moriarty, is once again plotting something diabolical. Sherlock's companion, Dr. Watson, suggests Moriarty may be responsible for MI6's recent issues with their supercomputer, The Beast.
+    Sherlock Holmes suspects his archenemy, Professor Moriarty, is once again plotting something diabolical. Sherlock's
+    companion, Dr. Watson, suggests Moriarty may be responsible for MI6's recent issues with their supercomputer, The Beast.
 
-Shortly after resolving to investigate, Sherlock receives a note from Moriarty boasting about infecting The Beast with a virus; however, he also gives him a clue—a number, NN. Sherlock determines the key to removing the virus is to find the largest Decent Number having NN digits.
+Shortly after resolving to investigate, Sherlock receives a note from Moriarty boasting about infecting 
+The Beast with a virus; however, he also gives him a clue—a number, NN. Sherlock determines the key to removing the virus 
+is to find the largest Decent Number having NN digits.
 
 A Decent Number has the following properties:
 
@@ -48,7 +51,8 @@ Its digits can only be 3's and/or 5's.
 The number of 3's it contains is divisible by 5.
 The number of 5's it contains is divisible by 3.
 If there are more than one such number, we pick the largest one.
-Moriarty's virus shows a clock counting down to The Beast's destruction, and time is running out fast. Your task is to help Sherlock find the key before The Beast is destroyed!
+Moriarty's virus shows a clock counting down to The Beast's destruction,
+and time is running out fast. Your task is to help Sherlock find the key before The Beast is destroyed!
 
 Constraints
 1≤T≤201≤T≤20
@@ -81,7 +85,10 @@ Sample Output
 Explanation
 
 For N=1N=1, there is no decent number having 11 digit (so we print −1−1).
-For N=3N=3, 555555 is the only possible number. The number 55 appears three times in this number, so our count of 55's is evenly divisible by 33 (Decent Number Property 3).
-For N=5N=5, 3333333333 is the only possible number. The number 33 appears five times in this number, so our count of 33's is evenly divisible by 55 (Decent Number Property 2).
-For N=11N=11, 5555553333355555533333 and all permutations of these digits are valid numbers; among them, the given number is the largest one.
+For N=3N=3, 555555 is the only possible number. The number 55 appears three times in this number, 
+so our count of 55's is evenly divisible by 33 (Decent Number Property 3).
+For N=5N=5, 3333333333 is the only possible number. The number 33 appears five times in this number, 
+so our count of 33's is evenly divisible by 55 (Decent Number Property 2).
+For N=11N=11, 5555553333355555533333 and all permutations of these digits are valid numbers; among them, 
+the given number is the largest one.
     */
