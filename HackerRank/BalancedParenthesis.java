7eb6c905bebfc70e327067d9cd3f9d494490b194
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2016-05-04 14:13:13 -0500

    Update BalancedParenthesis.java

--- HackerRank/BalancedParenthesis.java
+++ HackerRank/BalancedParenthesis.java
@@ -9,7 +9,6 @@ import java.util.regex.*;
 public class BalancedParenthesis {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         sc.nextLine();
