78fd1d6b47f4c3170cb71e294d0e0c1796e50ba1
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2016-07-08 21:20:16 -0500

    Create UniqueWordsInTwoStrings.java

---
+++ 1/1.1/UniqueWordsInTwoStrings/UniqueWordsInTwoStrings.java
@@ -0,0 +1,36 @@
+// Return a list of words that exists in one sentence but not on the other one.
+// for example s1 = "I am happy" s2 = "I am old", you have to return ["happy", "old"] 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class UniqueWordsInTwoStrings {
+	
+	public static void main(String[] args) {
+		String str1 = "I am happy";
+		String str2 = "I  am                          Bro";
+		String[] arr1 = str1.split("\\s+");
+		String[] arr2 = str2.split("\\s+");
+		ArrayList<String> list1 = new ArrayList<String>();
+		ArrayList<String> list2 = new ArrayList<String>();
+		for (String str : arr1) {
+			list1.add(str);
+		}
+		for (String str : arr2) {
+			list2.add(str);
+		}
+		ArrayList<String> list3 = new ArrayList<String>();
+		list3.addAll(list1);
+		list3.addAll(list2);
+		list1.retainAll(list2);
+		list2.retainAll(list1);
+		list3.removeAll(list1);
+		list3.removeAll(list2);
+		System.out.println(list3.toString());
+	}
+
+}
+
+// O/P
+// [happy, Bro]
