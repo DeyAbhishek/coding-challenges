d61b4b9b0f37227f4afae1c864ec62d8f4443c1c
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2017-02-19 16:06:37 -0600

    Update FindIfTheWholeWordCanBeBrokenIntoOneOrMoreDictionaryWords.java

--- 2017/Video/FindIfTheWholeWordCanBeBrokenIntoOneOrMoreDictionaryWords.java
+++ 2017/Video/FindIfTheWholeWordCanBeBrokenIntoOneOrMoreDictionaryWords.java
@@ -44,5 +44,17 @@ public class FindIfTheWholeWordCanBeBrokenIntoOneOrMoreDictionaryWords {
 		
 		return new Result("", false);
 	}
+	
+	
+	private class Result {
+		public String words;
+		public boolean res;
+
+		public Result(String words, boolean res) {
+			this.words = words;
+			this.res = res;
+		}
+	}
+
 
 }
