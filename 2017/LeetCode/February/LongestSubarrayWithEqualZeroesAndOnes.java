68a96414c8c6ac95b9e0fcd2a87206cc260d9ab6
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2017-02-20 19:34:29 -0600

    Update and rename ContiguousArray.java to LongestSubarrayWithEqualZeroesAndOnes.java

--- 2017/LeetCode/February/LongestSubarrayWithEqualZeroesAndOnes.java
+++ 2017/LeetCode/February/LongestSubarrayWithEqualZeroesAndOnes.java
@@ -22,8 +22,11 @@ The inner loop considers all subarrays starting from i.
 If size of a subarray is greater than maximum size so far, then update the maximum size.
 In the below code, 0s are considered as -1 and sum of all values from i to j is calculated. 
 If sum becomes 0, then size of this subarray is compared with largest size so far.
+
+Time Complexity => O(n * n)
+Space Complexity => O ( 1 )
 */
-class ContiguousArray {
+class LongestSubarrayWithEqualZeroesAndOnes {
  
     // This function Prints the starting and ending
     // indexes of the largest subarray with equal  
