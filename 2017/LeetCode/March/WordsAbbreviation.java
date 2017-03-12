478e6686f3419bb0929ccef19a67a2147cfefa4b
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2017-03-11 22:46:01 -0600

    Create WordsAbbreviation.java

---
+++ 2017/LeetCode/March/WordsAbbreviation.java
@@ -0,0 +1,44 @@
+/*
+527. Word Abbreviation My SubmissionsBack To Contest
+
+Difficulty: Hard
+Given an array of n distinct non-empty strings, you need to generate minimal possible abbreviations for every word following rules below.
+
+Begin with the first character and then the number of characters abbreviated, which followed by the last character.
+If there are any conflict, that is more than one words share the same abbreviation, a longer prefix is used instead of only the first character until making the map from word to abbreviation become unique. In other words, a final abbreviation cannot map to more than one original words.
+If the abbreviation doesn't make the word shorter, then keep it as original.
+Example:
+Input: ["like", "god", "internal", "me", "internet", "interval", "intension", "face", "intrusion"]
+Output: ["l2e","god","internal","me","i6t","interval","inte4n","f2e","intr4n"]
+Note:
+Both n and the length of each word will not exceed 400.
+The length of each word is greater than 1.
+The words consist of lowercase English letters only.
+The return answers should be in the same order as the original array.
+*/
+
+public class WordsAbbreviation {
+    public List<String> wordsAbbreviation(List<String> dict) {
+        int len=dict.size();
+        String[] ans=new String[len];
+        int[] cnt=new int[len];
+        for (int i=0;i<len;i++) {
+            cnt[i]=1;
+            ans[i]=makeString(dict.get(i), 1);
+        }
+        for (int i=0;i<len;i++) {
+            while (true) {
+                HashSet<Integer> set=new HashSet<>();
+                for (int j=i+1;j<len;j++) {
+                    if (ans[j].equals(ans[i])) set.add(j);
+                }
+                if (set.isEmpty()) break;
+                set.add(i);
+                for (int k: set) {
+                    cnt[k]++;
+                    ans[k]=makeString(dict.get(k), cnt[k]);
+                }
+            }
+        }
+        return Arrays.asList(ans);
+    }
