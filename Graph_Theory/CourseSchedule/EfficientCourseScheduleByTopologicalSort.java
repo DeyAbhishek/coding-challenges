b5951d1c0eb7da8fe016572fb44f2422a4390d11
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2017-11-14 16:29:55 -0600

    Update EfficientCourseScheduleByTopologicalSort.java

--- Graph_Theory/CourseSchedule/EfficientCourseScheduleByTopologicalSort.java
+++ Graph_Theory/CourseSchedule/EfficientCourseScheduleByTopologicalSort.java
@@ -1,28 +1,30 @@
 public class EfficientCourseScheduleByTopologicalSort {
-public boolean canFinish(int numCourses, int[][] prerequisites) {
-    int[] indegree = new int[numCourses];
-    Queue<Integer> queue = new LinkedList<Integer>();
-    for(int[] pair:prerequisites){
-        indegree[pair[1]]++;
-    }
-    for(int i=0;i<indegree.length;i++){
-        if(indegree[i]==0){
-            queue.add(i);
-        }
-    }
-    int select = 0;
-    while(!queue.isEmpty()){
-        numCourses--;
-        int course = queue.poll();
+    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        Queue<Integer> queue = new LinkedList<Integer>();
         for(int[] pair:prerequisites){
-            if(pair[0]==course){
-                indegree[pair[1]]--;
-                if(indegree[pair[1]]==0){
-                    queue.add(pair[1]);
+            indegree[pair[1]]++;
+        }
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                queue.add(i);
+            }
+        }
+        int select = 0;
+        while(!queue.isEmpty()){
+            numCourses--;
+            int course = queue.poll();
+            for(int[] pair:prerequisites){
+                if(pair[0]==course){
+                    indegree[pair[1]]--;
+                    if(indegree[pair[1]]==0){
+                        queue.add(pair[1]);
+                    }
                 }
             }
         }
+        return numCourses==0;
     }
-    return numCourses==0;
-}
+    
 }
