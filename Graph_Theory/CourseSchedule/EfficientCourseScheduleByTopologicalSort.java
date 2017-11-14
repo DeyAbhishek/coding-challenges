ec91e16dce2ffb407b57945bc074b40989dfdedc
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2017-11-14 16:29:34 -0600

    Create EfficientCourseScheduleByTopologicalSort.java

---
+++ Graph_Theory/CourseSchedule/EfficientCourseScheduleByTopologicalSort.java
@@ -0,0 +1,28 @@
+public class EfficientCourseScheduleByTopologicalSort {
+public boolean canFinish(int numCourses, int[][] prerequisites) {
+    int[] indegree = new int[numCourses];
+    Queue<Integer> queue = new LinkedList<Integer>();
+    for(int[] pair:prerequisites){
+        indegree[pair[1]]++;
+    }
+    for(int i=0;i<indegree.length;i++){
+        if(indegree[i]==0){
+            queue.add(i);
+        }
+    }
+    int select = 0;
+    while(!queue.isEmpty()){
+        numCourses--;
+        int course = queue.poll();
+        for(int[] pair:prerequisites){
+            if(pair[0]==course){
+                indegree[pair[1]]--;
+                if(indegree[pair[1]]==0){
+                    queue.add(pair[1]);
+                }
+            }
+        }
+    }
+    return numCourses==0;
+}
+}
