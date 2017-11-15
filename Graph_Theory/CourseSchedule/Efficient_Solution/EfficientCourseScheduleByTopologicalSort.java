86670bd617576e782f23ce8253c800a35035bcb8
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2017-11-15 16:32:32 -0600

    Update EfficientCourseScheduleByTopologicalSort.java

--- Graph_Theory/CourseSchedule/Efficient_Solution/EfficientCourseScheduleByTopologicalSort.java
+++ Graph_Theory/CourseSchedule/Efficient_Solution/EfficientCourseScheduleByTopologicalSort.java
@@ -1,17 +1,21 @@
 public class EfficientCourseScheduleByTopologicalSort {
     
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
         int[] indegree = new int[numCourses];
+        
         Queue<Integer> verticesWithNoInboundEdge = new LinkedList<Integer>();
+        
         for(int[] pair:prerequisites){
             indegree[pair[1]]++;
         }
+        
         for(int i=0;i<indegree.length;i++){
             if(indegree[i]==0){
                 verticesWithNoInboundEdge.add(i);
             }
         }
-        int select = 0;
+        
         while(!verticesWithNoInboundEdge.isEmpty()){
             numCourses--;
             int course = verticesWithNoInboundEdge.poll();
@@ -24,6 +28,7 @@ public class EfficientCourseScheduleByTopologicalSort {
                 }
             }
         }
+        
         return numCourses==0;
     }
     
